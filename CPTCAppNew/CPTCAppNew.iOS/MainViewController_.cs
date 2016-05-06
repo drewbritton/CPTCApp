262e9f7a712f7a4ba5fc989a2ba5e39f899316ce
Drew Britton <drewbritton@gmail.com>
2016-05-06 16:07:00 -0700

    Renamed NewViewController to MainViewController

---
+++ CPTCAppNew/CPTCAppNew.iOS/MainViewController_.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CPTCAppNew.iOS
+{
+	partial class MainViewController : UIViewController
+	{
+		public MainViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
